01a2764d78d7f9c86cd5c4a39ce10befc666c604
mallabonikki <mallabonikki6678@gmail.com>
2017-06-22 23:58:16 +1000

    Lightbox image overlay

--- js/script.js
+++ js/script.js
@@ -27,4 +27,42 @@ $(document).ready(function () {
         return false;
     });
 
+    $('ul#gallery li').mouseenter(function () {
+        console.log('mouse enter event');
+        //Get the data attribute values
+        var title = $(this).children().data('title');
+        var desc = $(this).children().data('desc');
+         
+        //validation
+        if (desc == null) {
+            desc = 'Click To Enlarge';
+        }
+
+        if (title == null) {
+            title = '';
+        }
+
+        // create overlay div
+        $(this).append('<div class="overlay"></div>');
+
+        // get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        //add html to overlay
+        overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+
+        overlay.fadeIn(800);
+    });
+
+    // Mouseleave overlay
+    $('ul#gallery li').mouseleave(function () {
+        // create overlay div
+        $(this).append('<div class="overlay"></div>');
+
+        // get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        //fade out overlay
+        overlay.fadeOut(500);
+    });
 });
